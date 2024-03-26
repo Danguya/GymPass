b9cebe8462c11d5d7ca174de99421cb7c585e0cc
danguya <daniel.yava16@gmail.com>
2024-03-26 22:22:19 +0100

    feat: organizando o app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Wilmy Danguya',
-    email: 'daniel.yava16@gmail.com',
-  },
-})
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(appRoutes)
