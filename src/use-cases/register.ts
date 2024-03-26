e4d85d37c04cb7912effd089f2e218a90f78d002
danguya <daniel.yava16@gmail.com>
2024-03-26 22:20:03 +0100

    feat: adicionando o use-case register

---
+++ src/use-cases/register.ts
@@ -0,0 +1,35 @@
+import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('E-mail already exists.')
+  }
+
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  })
+}
