b6c7d743bc1d6f05eb6235f47460b13ff7448b7f
danguya <daniel.yava16@gmail.com>
2024-03-27 00:24:04 +0100

    feat: escrevendo o primeiro teste unitario do projecto

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,34 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+    const isPasswordCorrectkyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+    expect(isPasswordCorrectkyHashed).toBe(true)
+  })
+})
