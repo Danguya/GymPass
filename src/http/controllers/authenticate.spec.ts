77e8892e5c19fa484f16a749328a922af6c24603
danguya <daniel.yava16@gmail.com>
2024-03-31 22:10:09 +0100

    test: adicionando os testes e2e do perfil

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,30 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('Auth', () => { })
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@eaxample.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      name: 'John Doe',
+      email: 'johndoe@eaxample.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
